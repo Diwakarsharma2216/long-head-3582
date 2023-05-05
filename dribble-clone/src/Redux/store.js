b5c99643bbcf5926e3cf7e202e937754e3845da9
Diwakar Sharma <diwakarsharma2216@gmail.com>
2023-05-05 12:27:09 +0530

    update-1

--- dribble-clone/src/Redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore,combineReducers,applyMiddleware } from 'redux'
-import { reducer as Authreducer } from './Authentication/reducer'
-import { reducer as Productreducer } from "./Products/reducer"
-import thunk from "redux-thunk"
-// const rootReducer =combineReducers({Authreducer,Productreducer})
-const rootReducer=combineReducers({Authreducer,Productreducer})
-export const store=createStore(rootReducer,applyMiddleware(thunk))
--- dribble-clone/src/index.js
+++ dribble-clone/src/index.js
@@ -4,17 +4,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./Redux/store";
-import { Provider } from "react-redux";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <ChakraProvider>
-    <Provider store={store}>
+   
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
+   
   </ChakraProvider>
 );
 
