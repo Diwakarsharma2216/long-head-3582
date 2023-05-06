63bef2fbd3579bd13d1bf4d2739fa1f14e7e660d
Diwakar Sharma <diwakarsharma2216@gmail.com>
2023-05-06 08:56:32 +0530

    update-1

--- dribble-clone/src/Pages/HomePage.jsx
+++ dribble-clone/src/Pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import MidSection from './MidSection'
 
 const HomePage = () => {
   return (
     <div>
-      
+      <MidSection />
     </div>
   )
 }
--- dribble-clone/src/Pages/MidSection.jsx
+++ dribble-clone/src/Pages/MidSection.jsx
@@ -1,9 +1,102 @@
+import { Container, SimpleGrid,Box, HStack, VStack,Text, Image, Heading, Button } from '@chakra-ui/react'
 import React from 'react'
-
+import { BsBoxArrowUpRight } from "react-icons/bs";
+import craditmeter from "../assets/meter.png"
+import { BsFillCreditCard2BackFill, BsThreeDots } from "react-icons/bs";
+import { BsThreeDotsVertical } from "react-icons/bs";
 const MidSection = () => {
+  const data=[{},{},{},{}]
   return (
     <div>
-      
+     <Container maxW={"container.xl"}  minH={"3xl"}  bg={"green.300"}  mt="50" >
+      <Box>
+        <SimpleGrid columns={{base:1,md:2}}>
+            <Box mt={"10"}>
+              <Box  >
+              <HStack spacing={"auto"}>
+                <Box width={"aut0"}>
+                  <Heading>Health Karma</Heading>
+                </Box>
+                <Box>
+                 <Button borderRadius={"3xl"}  ml={"1.5"} colorScheme='teal' variant='outline'>Explore</Button>
+                 <Button borderRadius={"3xl"}  ml={"1.5"} colorScheme='teal' variant='outline' >Transunion</Button>
+                </Box>
+              </HStack>
+              </Box>
+              <Box mt={"2.5"}   >
+              <HStack>
+                <Box>
+                  -1pts
+                  <Text fontSize={{base:"2xl",md:"8xl"}}>832</Text>
+                  Excellent Checked daily
+                </Box>
+                <Box>
+                  <Image src={craditmeter} height={{md:"md"}}></Image>
+                </Box>
+              </HStack>
+              </Box>
+
+
+              <Box  mt={"12.5"}   >
+              <HStack>
+                <Box bg={"whiteAlpha.900"} h={"36"} w="60"  borderRadius={"2xl"}>
+                  <VStack>
+                  <Box >
+                    <HStack spacing={"8"} >
+                    
+                      $<Text fontSize={"2xl"}>net Worth</Text>
+                      <Box> <BsBoxArrowUpRight /></Box>
+                    </HStack>
+                  <Text fontSize={"4xl"} ><span style={{fontSize:"20px",color:"grey"}} >$</span>137,036</Text>
+                  </Box>
+
+                  </VStack>
+                  
+                </Box>
+                <Box bg={"whiteAlpha.900"} h={"36"} w="60"  borderRadius={"2xl"}>
+                  <VStack>
+                  <Box >
+                    <HStack spacing={"8"} >
+                    
+                      $<Text fontSize={"2xl"}>net Worth</Text>
+                      <Box fontsize="3xl"> <BsBoxArrowUpRight /></Box>
+                    </HStack>
+                  <Text fontSize={"4xl"} ><span style={{fontSize:"20px",color:"grey"}} >$</span>137,036</Text>
+                  </Box>
+
+                  </VStack>
+                  
+                </Box>
+              </HStack>
+              </Box>
+
+
+            </Box>
+            <Box  ml={{md:"16"}}> 
+              <SimpleGrid columns={{base:1,md:2}} gap={{base:"1",md:"2"}} >
+               {data.map((el)=><Box bg={"whiteAlpha.900"} w={{base:"auto",md:"64"}} h={{base:"185",md:"185"}} borderRadius={"2xl"}>
+                <VStack>
+                <HStack  spacing={"40"} mt={"5"} ml="5">
+               <Box borderRadius={"3xl"} bg={"green.300"} fontSize={"3xl"} ><BsFillCreditCard2BackFill /></Box>
+               <Box borderRadius={"3xl"} mt={"5"} mr="10" border={"1px solid grey"} bg={"white.300"} fontSize={"3xl"}><BsThreeDotsVertical /></Box>
+                
+                </HStack>
+                <Text textAlign={"left"} fontSize={"xl"} fontStyle={"bold"}> Credit <br></br> 
+                Card Use</Text>
+                <HStack  spacing={"40"} mt={"5"} ml="5">
+                 <Text   fontSize={"3xl"}>9</Text>
+                 <Box fontSize={"3xl"}> <BsThreeDots /> </Box>
+                
+                </HStack>
+                </VStack>
+               
+               </Box>)}
+               </SimpleGrid>
+            </Box>
+        </SimpleGrid>
+      </Box>
+
+     </Container>
     </div>
   )
 }
