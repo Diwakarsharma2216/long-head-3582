43e59d9a86e8f0c745dbc64ecc01b23bd690a7d7
Diwakar Sharma <diwakarsharma2216@gmail.com>
2023-05-06 20:47:08 +0530

    update-1

--- dribble-clone/src/App.js
+++ dribble-clone/src/App.js
@@ -1,12 +1,14 @@
 
 import MainRoutes from "./Pages/MainRoutes";
-import Navbar from "./Components/Navbar";
+
+
 
 function App() {
   return (
     <div>
-      <Navbar/>
+     
       <MainRoutes />
+     
     </div>
   );
 }
