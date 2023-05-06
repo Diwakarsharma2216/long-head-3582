412c25162bb2b7a8b4006a99d712849430069894
Diwakar Sharma <diwakarsharma2216@gmail.com>
2023-05-06 09:00:49 +0530

    update-2

--- dribble-clone/src/Pages/HomePage.jsx
+++ dribble-clone/src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MidSection from './MidSection'
+import CreditOffers from '../Components/CreditOffers'
 
 const HomePage = () => {
   return (
     <div>
       <MidSection />
+      <CreditOffers />
     </div>
   )
 }
